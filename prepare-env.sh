88a28b18eca15a2bc6026b8d296993fde6fdcd26
angryrobotdeals <121421580+angryrobotdeals@users.noreply.github.com>
2024-03-09 15:40:58 +0300

    add dataset #2

---
+++ prepare-env.sh
@@ -0,0 +1,5 @@
+pip install git+https://github.com/keras-team/keras-tuner.git
+pip install -U autokeras pandas numpy scikit-learn GPUtil humanize psutil matplotlib absl-py
+
+# for Mac M1
+#pip install -U tensorflow-metal
