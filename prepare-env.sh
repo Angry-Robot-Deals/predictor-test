edcd3cad06c5260949959260fb04f048c5a1f3de
angryrobotdeals <121421580+angryrobotdeals@users.noreply.github.com>
2024-03-09 16:03:12 +0300

    add dataset #6

--- prepare-env.sh
+++ prepare-env.sh
@@ -1,3 +1,6 @@
+git lfs pull --all
+git lfs pull --ref origin/master
+
 pip install git+https://github.com/keras-team/keras-tuner.git
 pip install -U autokeras pandas numpy scikit-learn GPUtil humanize psutil matplotlib absl-py
 
